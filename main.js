5a19864a1ccca684844b2f43726b98599d7edcdd
Karol <pandamax.karol.lichon@gmail.com>
2023-01-25 15:40:46 +0100

    under_science_begin--password-validator-easy-level

---
+++ main.js
@@ -0,0 +1,40 @@
+const pass = document.querySelector('#password')
+const p = document.querySelector('.passinfo')
+const letters = /[a-z]/i
+const numbers = /[0-9]/
+const special = /[!@#$%^&*()]/
+const minValue = 10
+
+/// be carefull, it's easy to make a mistake
+const checkPassword = () => {
+	if (
+		pass.value.length > minValue &&
+		pass.value.match(letters) &&
+		pass.value.match(numbers) &&
+		pass.value.match(special)
+	) {
+		p.innerHTML = 'Masz bardzo dobre hasło :)'
+		p.style.color = 'lime'
+	} else if (
+		pass.value.length > minValue &&
+		pass.value.match(letters) &&
+		pass.value.match(numbers)
+	) {
+		p.innerHTML = 'Masz dobre hasło'
+		p.style.color = 'gold'
+	} else {
+		p.innerHTML = 'Masz za słabe hasło'
+		p.style.color = 'tomato'
+	}
+}
+
+// .value  wartośc jaką przechowuje
+pass.addEventListener('keyup', function () {
+	if (pass.value !== '') {
+		checkPassword()
+	} else {
+		p.innerHTML = 'Nie podałeś hasła...'
+		p.removeAttribute('style')
+		// p.style.color = 'lime' //usuwamy atrybut gorsze
+	}
+})
